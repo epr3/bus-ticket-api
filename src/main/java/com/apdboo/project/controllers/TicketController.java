2f2d5f99b2a35494f55f1b18e459baf7ac5d721c
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-06-27 00:11:34 +0300

    removed unused packages

--- src/main/java/com/apdboo/project/controllers/TicketController.java
+++ src/main/java/com/apdboo/project/controllers/TicketController.java
@@ -1,23 +1,13 @@
 package com.apdboo.project.controllers;
 
-import com.apdboo.project.exceptions.*;
-import com.apdboo.project.models.*;
-import com.apdboo.project.repositories.BusRepository;
-import com.apdboo.project.repositories.IntervalRepository;
-import com.apdboo.project.repositories.RouteRepository;
-import com.apdboo.project.repositories.TicketRepository;
-import com.apdboo.project.requests.CityRequest;
 import com.apdboo.project.requests.TicketRequest;
 import com.apdboo.project.services.TicketService;
-import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/tickets")
